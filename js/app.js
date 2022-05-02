5e75c08c16d50e6399090f4f28d17b001c46ccb9
Jose david Redondo <redondoperdomojosedavid@gmail.com>
2022-05-01 23:57:08 -0500

    add carrusel

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 const $menu = document.querySelector(".nav"),
-  $icon = document.getElementById("icon");
+  $icon = document.getElementById("icon"),
+  $cards = document.querySelector(".recommendation__cards");
+let posicion = 0;
 
 document.addEventListener("click", (e) => {
   if (e.target === $icon) {
     $menu.classList.toggle("is__active");
+  }
+  if (e.target === right) {
+    if (posicion === 66.66) return;
+    posicion += 33.33;
+    console.log(posicion);
+    $cards.style.transform = `translateX(-${posicion}%)`;
+  }
+  if (e.target === left) {
+    if (posicion === 0) return;
+    posicion -= 33.33;
+    console.log(posicion);
+    $cards.style.transform = `translateX(-${posicion}%)`;
   }
 });
